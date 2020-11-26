1f3f5fb0c3a0714968af2fc4001e9661a492a0af
Victor <vic8.taillieu@gmail.com>
2020-11-26 23:48:59 +0100

    Implement linear regression class

--- TP1/linreg.py
+++ TP1/linreg.py
@@ -1,90 +1,87 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import linear_model
 
 
-def preprocessingData(file):
-    data = pd.read_excel(file)
-    X = np.asmatrix(data.iloc[:, :-1])
-    m, n = X.shape
-    X = np.insert(X, 0, 1, axis=1)
-    y = np.asmatrix(data.iloc[:, -1:])
-    w = np.array([[1] for i in range(n + 1)])
+class LinearRegression:
+    def __init__(self, data):
+        self.X, self.y, self.w, self.m, self.n = self.preprocessingData(data)
 
-    normalizeFeatures(X, n)
+        self.MAE = None
+        self.RMSE = None
+        self.R2 = None
 
-    return X, y, w, m, n
+    def preprocessingData(self, df):
+        X = self.normalizeFeatures(df.iloc[:, :-1])
+        X = np.asmatrix(X)
+        m, n = X.shape
+        X = np.insert(X, 0, 1, axis=1)
+        y = np.asmatrix(df.iloc[:, -1:])
+        w = np.array([[1] for i in range(n + 1)])
 
+        return X, y, w, m, n
 
-def normalizeFeatures(X, n):
-    for feat in range(1, n + 1):
-        min = np.min(X[:, feat])
-        max = np.max(X[:, feat])
-        X[:, feat] = (X[:, feat] - min) / (max - min)
+    def normalizeFeatures(self, X):
+        self.scaling = list(zip(X.min(), X.max()))
 
+        return (X - X.min()) / (X.max() - X.min())
 
-def costFunction(X, y, w, m):
-    return float(1 / (2 * m) * (X * w - y).T * (X * w - y))
+    def costFunction(self, X, y, w, m):
+        return float(1 / (2 * m) * (X * w - y).T * (X * w - y))
 
+    def gradientDescent(self, alpha=0.03, threshold=1e-3, iter=1000, autoAlpha=True):
+        i = 0
+        self.J = []
 
-def gradientDescent(file, alpha=0.03, threshold=1e-3, iter=1000):
-    X, y, w, m, n = preprocessingData(file)
+        while True:
+            i += 1
+            self.J.append(self.costFunction(self.X, self.y, self.w, self.m))
 
-    J = []
-    i = 0
-    while True:
-        J.append(costFunction(X, y, w, m))
-        delta = 1 / m * (X.T * X * w - X.T * y)
-        w = w - alpha * delta
-        i += 1
+            grad = 1 / self.m * (self.X.T * self.X * self.w - self.X.T * self.y)
+            self.w = self.w - alpha * grad
 
-        if (abs(costFunction(X, y, w, m) - J[-1]) < threshold) or (i == iter):
-            break
+            if len(self.J) > 1:
+                if autoAlpha and self.J[-1] > self.J[-2]:
+                    alpha /= 1.1
 
-    print("\nBuild regression")
-    print("Coefficients:\n", w)
-    print(abs(costFunction(X, y, w, m) - J[-1]))
-    print("MAE:", meanAbsoluteError(X, y, w, m))
-    print("RMSE:", rootMeanSquaredError(X, y, w, m))
-    print("R2:", r2(X, y, w, m))
+                if abs(self.J[-1] - self.J[-2]) < threshold or i == iter:
+                    break
 
-    print("\nNormal equation:")
-    print(normalEquation(X, y))
+        self.updateScores()
 
-    plt.plot(np.log(J))
-    plt.title("Value of the cost function over iterations")
-    plt.xlabel("iteration")
-    plt.ylabel("log of cost function value")
-    plt.show()
+    def plotCost(self):
+        try:
+            plt.plot(np.log(self.J))
+            plt.title("Value of the cost function over iterations")
+            plt.xlabel("iteration")
+            plt.ylabel("log of cost function value")
+            plt.show()
+        except AttributeError:
+            print("No gradient descent was performed")
 
+    def normalEquation(self):
+        self.w = (self.X.T * self.X)**-1 * self.X.T * self.y
 
-def meanAbsoluteError(X, y, w, m):
-    return float(1 / m * sum(abs(X * w - y)))
+        self.updateScores()
 
+    def meanAbsoluteError(self, X, y, w, m):
+        return float(1 / m * sum(abs(X * w - y)))
 
-def rootMeanSquaredError(X, y, w, m):
-    return float(1 / m * (X * w - y).T * (X * w - y))**(1 / 2)
+    def rootMeanSquaredError(self, X, y, w, m):
+        return float(1 / m * (X * w - y).T * (X * w - y))**(1 / 2)
 
+    def r2(self, X, y, w, m):
+        return float(((X * w - np.mean(y)).T * (X * w - np.mean(y))) / ((y - np.mean(y)).T * (y - np.mean(y))))
 
-def r2(X, y, w, m):
-    return float(((X * w - np.mean(y)).T * (X * w - np.mean(y))) / ((y - np.mean(y)).T * (y - np.mean(y))))
+    def updateScores(self):
+        self.MAE = self.meanAbsoluteError(self.X, self.y, self.w, self.m)
+        self.RMSE = self.rootMeanSquaredError(self.X, self.y, self.w, self.m)
+        self.R2 = self.r2(self.X, self.y, self.w, self.m)
 
+    def predict(self, test):
+        X_test = np.asmatrix(test)
+        minmax = list(zip(*self.scaling))
+        min, max = np.array(minmax[0]), np.array(minmax[1])
+        X_test = (X_test - min) / (max - min)
 
-def normalEquation(X, y):
-    return (X.T * X)**-1 * X.T * y
-
-
-gradientDescent("../data/CCPP.xlsx", alpha=0.9)
-
-X, y, w, m, n = preprocessingData("../data/CCPP.xlsx")
-print("Scikit-learn")
-reg = linear_model.LinearRegression()
-reg.fit(X, y)
-s = reg.coef_
-s[0, 0] = reg.intercept_
-s = s.T
-print(s)
-print("MAE:", meanAbsoluteError(X, y, s, m))
-print("RMSE:", rootMeanSquaredError(X, y, s, m))
-print("R2:", r2(X, y, s, m))
+        return X_test * self.w[1:] + self.w[0]
