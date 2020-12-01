fd8d22bc4019484f8aafba526cfed5d636a59a8c
Victor <vic8.taillieu@gmail.com>
2020-12-01 12:28:46 +0100

    Update linear regression class (train/test sets)

--- Lab1/linreg.py
+++ Lab1/linreg.py
@@ -1,30 +1,33 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
 class LinearRegression:
-    def __init__(self, data):
-        self.X, self.y, self.w, self.m, self.n = self.preprocessingData(data)
+    def __init__(self, data, test_size=0.2, random_state=42):
+        self.preprocessingData(data, test_size, random_state)
 
-        self.MAE = None
-        self.RMSE = None
-        self.R2 = None
+    def preprocessingData(self, df, test_size, random_state):
+        X = df.iloc[:, :-1]
+        y = df.iloc[:, -1:]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+        self.m, self.n = X_train.shape
+        self.X_train = self.normalizeFeatures(X_train, fit=True)
+        self.X_test = self.normalizeFeatures(X_test)
+        self.y_train = np.asmatrix(y_train)
+        self.y_test = np.asmatrix(y_test)
+        self.w = np.asmatrix([[1] for i in range(self.n + 1)])
 
-    def preprocessingData(self, df):
-        X = self.normalizeFeatures(df.iloc[:, :-1])
-        X = np.asmatrix(X)
-        m, n = X.shape
-        X = np.insert(X, 0, 1, axis=1)
-        y = np.asmatrix(df.iloc[:, -1:])
-        w = np.array([[1] for i in range(n + 1)])
+    def normalizeFeatures(self, X, fit=False):
+        if fit:
+            self.scaling = list(zip(X.min(), X.max()))
 
-        return X, y, w, m, n
+        minmax = list(zip(*self.scaling))
+        min, max = np.array(minmax[0]), np.array(minmax[1])
+        X = np.asmatrix((X - min) / (max - min))
 
-    def normalizeFeatures(self, X):
-        self.scaling = list(zip(X.min(), X.max()))
-
-        return (X - X.min()) / (X.max() - X.min())
+        return np.insert(X, 0, 1, axis=1)
 
     def costFunction(self, X, y, w, m):
         return float(1 / (2 * m) * (X * w - y).T * (X * w - y))
@@ -35,9 +38,9 @@ class LinearRegression:
 
         while True:
             i += 1
-            self.J.append(self.costFunction(self.X, self.y, self.w, self.m))
+            self.J.append(self.costFunction(self.X_train, self.y_train, self.w, self.m))
 
-            grad = 1 / self.m * (self.X.T * self.X * self.w - self.X.T * self.y)
+            grad = 1 / self.m * (self.X_train.T * self.X_train * self.w - self.X_train.T * self.y_train)
             self.w = self.w - alpha * grad
 
             if len(self.J) > 1:
@@ -60,7 +63,7 @@ class LinearRegression:
             print("No gradient descent was performed")
 
     def normalEquation(self):
-        self.w = (self.X.T * self.X)**-1 * self.X.T * self.y
+        self.w = (self.X_train.T * self.X_train)**-1 * self.X_train.T * self.y_train
 
         self.updateScores()
 
@@ -70,18 +73,20 @@ class LinearRegression:
     def rootMeanSquaredError(self, X, y, w, m):
         return float(1 / m * (X * w - y).T * (X * w - y))**(1 / 2)
 
-    def r2(self, X, y, w, m):
-        return float(((X * w - np.mean(y)).T * (X * w - np.mean(y))) / ((y - np.mean(y)).T * (y - np.mean(y))))
+    def r2(self, X, y, w):
+        return float(1 - ((y - X * w).T * (y - X * w)) / ((y - y.mean()).T * (y - y.mean())))
 
     def updateScores(self):
-        self.MAE = self.meanAbsoluteError(self.X, self.y, self.w, self.m)
-        self.RMSE = self.rootMeanSquaredError(self.X, self.y, self.w, self.m)
-        self.R2 = self.r2(self.X, self.y, self.w, self.m)
+        self.MAE = self.meanAbsoluteError(self.X_train, self.y_train, self.w, self.m)
+        self.RMSE = self.rootMeanSquaredError(self.X_train, self.y_train, self.w, self.m)
+        self.R2 = self.r2(self.X_train, self.y_train, self.w)
 
-    def predict(self, test):
-        X_test = np.asmatrix(test)
-        minmax = list(zip(*self.scaling))
-        min, max = np.array(minmax[0]), np.array(minmax[1])
-        X_test = (X_test - min) / (max - min)
+        self.test_MAE = self.meanAbsoluteError(self.X_test, self.y_test, self.w, self.X_test.shape[0])
+        self.test_RMSE = self.rootMeanSquaredError(self.X_test, self.y_test, self.w, self.X_test.shape[0])
+        self.test_R2 = self.r2(self.X_test, self.y_test, self.w)
 
-        return X_test * self.w[1:] + self.w[0]
+    def predict(self, X_pred):
+        if X_pred.shape[1] < self.n + 1:
+            X_pred = self.normalizeFeatures(X_pred)
+
+        return X_pred * self.w
